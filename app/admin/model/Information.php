e64a0d931949e75f640385c5842d35a7dca0c981
iActing <758246061@qq.com>
2019-09-21 13:59:30 +0800

    最新消息前台页面接口

---
+++ app/admin/model/Information.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace app\admin\model;
+
+use \think\Model;
+use \think\Db;
+class Information extends Model
+{
+    public function getlist($number, $limit){
+        $data = Db::name('information')->order('sort desc')->limit($number, $limit)->select();
+        foreach($data as $k => $v){
+            $data[$k]['create_time'] = date('Y-m-d H:i:s', $data[$k]['create_time']);
+            $data[$k]['update_time'] = date('Y-m-d H:i:s', $data[$k]['update_time']);
+        }
+        return $data;
+    }
+
+    public function search($number, $limit, $q){
+        $data = Db::name('information')->whereLike('title', '%'.$q.'%')->order('sort desc')->limit($number, $limit)->select();
+        foreach($data as $k => $v){
+            $data[$k]['create_time'] = date('Y-m-d H:i:s', $data[$k]['create_time']);
+            $data[$k]['update_time'] = date('Y-m-d H:i:s', $data[$k]['update_time']);
+        }
+        return $data;
+    }
+
+    public function total(){
+        $data = Db::name('information')->field('count(id)')->find();
+        return $data['count(id)'];
+    }
+
+    public function edit($id, $sort, $img, $title, $content){
+        //入库操作
+        if (!empty($id)){
+            $update = [
+                'sort' => $sort,
+                'img' => $img,
+                'title' => $title,
+                'content' => $content,
+                'update_time' => time()
+            ];
+            $res = Db::name('information')->where('id', $id)->update($update);
+        }else{
+            $update = [
+                'sort' => $sort,
+                'img' => $img,
+                'title' => $title,
+                'content' => $content,
+                'update_time' => time(),
+                'create_time' => time()
+            ];
+            $res = Db::name('information')->insert($update);
+        }
+        return $res;
+    }
+
+    public function del($id){
+        $res = Db::name('information')->where('id', $id)->delete();
+        return $res;
+    }
+}
---
+++ app/api/model/Information.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace app\api\model;
+
+
+use think\Model;
+use think\Db;
+
+class Information extends Model
+{
+    public function getList($number, $limit){
+        $list = Db::name('information')->field(['img', 'title', 'content'])->order('sort asc')->limit($number, $limit)->select();
+        if(!empty($list)){
+            return ['status'=>1, 'data'=>$list, 'msg'=>'获取消息成功'];
+        }else{
+            return ['status'=>0, 'data'=>[], 'msg'=>'暂无消息'];
+        }
+    }
+
+    public function detail($id){
+        $list = Db::name('information')->field(['img', 'title', 'content'])->where('id', $id)->find();
+        if(!empty($list)){
+            return ['status'=>1, 'data'=>$list, 'msg'=>'获取最新消息成功'];
+        }else{
+            return ['status'=>0, 'data'=>[], 'msg'=>'暂无最新消息'];
+        }
+    }
+}
