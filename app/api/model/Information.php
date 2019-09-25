a1d4372de9bd933e4cbf0dd9a1ecef181111a007
iActing <758246061@qq.com>
2019-09-25 15:34:20 +0800

    参数id

--- app/api/model/Information.php
+++ app/api/model/Information.php
@@ -10,7 +10,7 @@ use think\Db;
 class Information extends Model
 {
     public function getList($number, $limit){
-        $list = Db::name('information')->field(['img', 'title', 'content'])->order('sort asc')->limit($number, $limit)->select();
+        $list = Db::name('information')->field(['id', 'img', 'title', 'content'])->order('sort asc')->limit($number, $limit)->select();
         foreach($list as $k => $v){
             $list[$k]['img'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]" . str_replace("\\",'/',$v['img']);
         }
