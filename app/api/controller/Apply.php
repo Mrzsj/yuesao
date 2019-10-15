f87f4fc50586b3e6c4d16c7312181437c7cb405c
iActing <758246061@qq.com>
2019-10-15 10:54:54 +0800

    请假类型

--- app/api/controller/Apply.php
+++ app/api/controller/Apply.php
@@ -16,16 +16,15 @@ class Apply
         $end_time = input('end_time');
         $reason = input('reason');
         $Apply_model = new Apply_model();
-        $where = [
-            'user_id' => $user_id,
-            'status' => 1
-        ];
-        $list = Db::name('order')->where($where)->select();
         if (!empty($type) && is_numeric($type)){
+            $where = [
+                'user_id' => $user_id,
+                'status' => 1
+            ];
+            $list = Db::name('order')->where($where)->select();
             foreach ($list as $k => $v){
                 if ((strtotime($start_time) >= $list[$k]['start_time']) && (strtotime($start_time) <= $list[$k]['end_time']) && (strtotime($start_time) <= strtotime($end_time))){
                     $data = $Apply_model->add($user_id, $list[$k]['matron_id'], $list[$k]['ordersn'], $type, $reason, $start_time, $end_time);
-                    var_dump($data);exit();
                     if ($data == 1) {
                         showjson(['status' => 1, 'msg' => '申请成功']);
                     } else {
