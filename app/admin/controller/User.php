bbcd10f469b1384349c26ea6122a0ac2c46a4fbb
iActing <758246061@qq.com>
2019-10-15 16:15:24 +0800

    用户信息

---
+++ app/admin/controller/User.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace app\admin\controller;
+
+
+use think\Db;
+
+class User extends Permissions
+{
+    public function index(){
+        return $this->fetch();
+    }
+
+    public function lists(){
+        $page = input('page');
+        $limit = input('limit');
+        $nickname = input('nickname');
+        if (empty($page) || !is_numeric($page)) {
+            showjson(['status'=>0,'msg'=>'请输入正确的页码']);
+        }
+        if (empty($limit) || !is_numeric($limit)) {
+            showjson(['status'=>0,'msg'=>'请输入正确的条数']);
+        }
+        $number = ($page - 1) * $limit;
+        if (empty($nickname)){
+            $list = Db::name('user')->field('id, openid, nickname, avatar_url, type, create_time')->order('create_time desc')->limit($number, $limit)->select();
+        }else{
+            $list = Db::name('user')->whereLike('nickname', '%'.$nickname.'%')->field('id, openid, nickname, avatar_url, type, create_time')->order('create_time desc')->limit($number, $limit)->select();
+        }
+        foreach($list as $k => $v){
+            $list[$k]['create_time'] = date('Y-m-d H:i:s', $list[$k]['create_time']);
+        }
+        $data = Db::name('information')->field('count(id)')->find();
+        $count = $data['count(id)'];
+        showjson(['code' => 0,'count' => $count,'data' => $list]);
+    }
+
+}
