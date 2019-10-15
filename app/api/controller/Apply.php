193ef89aacf5736071b60a1307eba23dd404f712
iActing <758246061@qq.com>
2019-10-15 10:49:26 +0800

    请假类型

--- app/api/controller/Apply.php
+++ app/api/controller/Apply.php
@@ -16,13 +16,12 @@ class Apply
         $end_time = input('end_time');
         $reason = input('reason');
         $Apply_model = new Apply_model();
+        $where = [
+            'user_id' => $user_id,
+            'status' => 1
+        ];
+        $list = Db::name('order')->where($where)->select();
         if (!empty($type) && is_numeric($type)){
-            $where = [
-                'user_id' => $user_id,
-                'status' => 1
-            ];
-            $list = Db::name('order')->where($where)->select();
-            $data = 0;
             foreach ($list as $k => $v){
                 if ((strtotime($start_time) >= $list[$k]['start_time']) && (strtotime($start_time) <= $list[$k]['end_time']) && (strtotime($start_time) <= strtotime($end_time))){
                     $data = $Apply_model->add($user_id, $list[$k]['matron_id'], $list[$k]['ordersn'], $type, $reason, $start_time, $end_time);
